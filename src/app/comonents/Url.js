597184ada381ab118984835996b6f20ebc5c8c81
Aftab35520 <161050400+Aftab35520@users.noreply.github.com>
2025-07-13 20:08:17 +0530

    'init'

--- src/app/comonents/Url.js
+++ src/app/comonents/Url.js
@@ -1,4 +1,4 @@
-const LiveUrl='http://localhost:4000/'
-const LocalHostUrl='https://assignmentaiserver.onrender.com/'
+const LocalHostUrl='http://localhost:4000/'
+const LiveUrl='https://assignmentaiserver.onrender.com/'
 
 export default LiveUrl
